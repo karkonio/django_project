9d32781d86f392f31257c891a7f2c47cec9b2f36
KorkaYess <karina.yesbukenova@gmail.com>
2019-10-18 17:49:00 +0600

    Update PostsSerializer with username instead id

--- django_project/api.py
+++ django_project/api.py
@@ -1,23 +1,36 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework import routers, permissions
+from rest_framework import routers
 
 from .models import Post, Profile
-from .serializers import PostSerializer, ProfileSerializer
+from .serializers import \
+    PostSerializer, ProfileSerializer, ProfileDetailSerializer
+
+
+class MultiSerializerViewSetMixin(object):
+    def get_serializer_class(self):  # pragma: no cover
+        try:
+            return self.serializer_action_classes[self.action]
+        except (KeyError, AttributeError):
+            return super(MultiSerializerViewSetMixin, self).get_serializer_class()  # noqa
 
 
 class PostViewSet(ModelViewSet):
     serializer_class = PostSerializer
     queryset = Post.objects.all()
+    read_only = True
 
 
-class ProfileViewSet(ModelViewSet):
-    serializer_class = ProfileSerializer
+class ProfileViewSet(MultiSerializerViewSetMixin, ModelViewSet):
     queryset = Profile.objects.all()
-
-    permission_classes = (permissions.IsAuthenticated,)
+    serializer_class = ProfileSerializer
+    serializer_action_classes = {
+        'list': ProfileSerializer,
+        'retrieve': ProfileDetailSerializer,
+    }
+    read_only = True
 
 
 router = routers.SimpleRouter()
-router.register(r'posts', PostViewSet)
-router.register(r'profiles', ProfileViewSet)
+router.register('posts', PostViewSet)
+router.register('profiles', ProfileViewSet)
 api_urls = router.urls
--- django_project/serializers.py
+++ django_project/serializers.py
@@ -3,13 +3,21 @@ from rest_framework import serializers
 from .models import Post, Profile
 
 
-class PostSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Post
-        fields = '__all__'
-
-
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
-        fields = '__all__'
+        fields = [
+            'id', 'username', 'birthday', 'first_name', 'last_name', 'age',
+            'zodiac', 'phone', 'website', 'city', 'posts'
+        ]
+
+class PostSerializer(serializers.ModelSerializer):
+    profile = serializers.SlugRelatedField(read_only=True, slug_field='username')
+
+    class Meta:
+        model = Post
+        fields = ['id', 'image', 'description', 'profile']
+
+
+class ProfileDetailSerializer(ProfileSerializer):
+    posts = PostSerializer(many=True)
