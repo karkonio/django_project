eb0784ecf5385c409ce6bf1e33ea0dba0687c6c0
KorkaYess <44070905+KorkaYess@users.noreply.github.com>
2019-11-05 14:19:45 +0600

    Rewrite auth and update tests for login (#11)
    * Rewrite auth and update tests for login

--- django_project/tests/functional/test_api.py
+++ django_project/tests/functional/test_api.py
@@ -1,10 +1,33 @@
-from rest_framework.authtoken.models import Token
-from rest_framework.test import APIClient
+from rest_framework.test import APIRequestFactory
+from django_project.api import Login
 
 
-def test_login_api(db, data):
-    user = data[0]
-    token = Token.objects.get(user=user)
-    client = APIClient()
-    print(client)
-    client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+# test login with correct credentials
+def test_login_post_method_ok(db, data):
+    profile, photo_1, photo_2, follow1, follow2, token = data
+    view = Login.as_view()
+    factory = APIRequestFactory()
+    request = factory.post(
+        '/api/auth/',
+        {"username": "user", "password": "useruser"},
+        format='json'
+    )
+    response = view(request)
+    assert response.status_code == 200
+    assert response.data == {'user_id': profile.id, 'token': token.key}
+
+
+# test login with wrong password
+def test_login_post_method_fail(db, data):
+    view = Login.as_view()
+    factory = APIRequestFactory()
+    request = factory.post(
+        '/api/auth/',
+        {"username": "test_user", "password": "userfuser"},
+        format='json'
+    )
+    response = view(request)
+    assert response.status_code == 400
+    response.render()
+    err_msg = b'{"non_field_errors":["Unable to log in with provided credentials."]}'  # noqa
+    assert response.content == err_msg
